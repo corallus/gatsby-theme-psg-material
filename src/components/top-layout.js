e489af73883a943eb348a79b473c6ce932bbbe8b
Vincent van Bergen <v.vanbergen@gmail.com>
2021-11-04 22:46:21 +0100

    fix

--- src/components/top-layout.js
+++ src/components/top-layout.js
@@ -6,7 +6,7 @@ import {Helmet} from "react-helmet";
 import {EventProvider} from "gatsby-theme-psg/src/components/Events/Context";
 
 import Viewport from './viewport';
-import 'App.css'
+import '../App.css'
 
 export default function TopLayout({ children, theme }) {
     return <>
