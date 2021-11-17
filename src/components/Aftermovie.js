c67451a6a32bfaf2c8959ac9ebfab6ea47eb55e2
Vincent van Bergen <v.vanbergen@gmail.com>
2021-11-17 20:58:57 +0100

    aftermovie conditional

--- src/components/Aftermovie.js
+++ src/components/Aftermovie.js
@@ -9,21 +9,24 @@ const Aftermovie = ({children, ...props}) => {
 
     const {state} = useContext(Context)
 
+    const aftermovie = state.event.frontmatter.aftermovie
+
+    console.log(aftermovie)
+
     return (
+        aftermovie &&
         <>
             <IconButton onClick={() => setOpen(true)} size="large">
-                <PlayCircleOutline />
+                <PlayCircleOutline/>
             </IconButton>
             <Typography>
                 Bekijk de aftermovie
             </Typography>
             <Dialog onClose={() => setOpen(false)} aria-labelledby="aftermovie-dialog" open={open}>
-                {state.event.frontmatter.aftermovie &&
-                <iframe width="560" height="315" src={`${state.event.frontmatter.aftermovie.replace('watch?v=', 'embed/')}?autoplay=1`}
+                <iframe width="560" height="315" src={`${aftermovie.replace('watch?v=', 'embed/')}?autoplay=1`}
                         title="YouTube video player" frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                         allowFullScreen />
-                }
             </Dialog>
         </>
     );
